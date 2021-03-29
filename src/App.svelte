2072305f3142531896d26105c5b7d717295e2483
timdeschryver <28659384+timdeschryver@users.noreply.github.com>
2021-03-29 17:45:11 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+  
+<script>
+	let variable = import.meta.env.MODE
+</script>
+
+<main>
+	<h1>Variable: {variable}</h1>
+</main>
+
+<!-- comment or remove styles and it works -->
+<style>
+	h1 {
+		color:red
+	}
+</style>
